1d5ed0afc4f0661b85041709f636ab340b113123
Lucia Agha Zadeh Dehdeh <azd.lucia@gmail.com>
2023-05-13 19:20:29 -0300

    ajuste

--- src/test/java/CalendarioTest.java
+++ src/test/java/CalendarioTest.java
@@ -12,8 +12,6 @@ import java.util.List;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.reflect.TypeToken;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
